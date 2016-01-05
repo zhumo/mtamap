fa6dc1664c75767b2e4ac9b4bc338c73c0c15467
Elliot <elliot.schrock@gmail.com>
2016-01-05 10:32:00 -0500

    yext

--- src/main/java/com/rndapp/mtamap/MtaActivity.java
+++ src/main/java/com/rndapp/mtamap/MtaActivity.java
@@ -1,14 +1,21 @@
 package com.rndapp.mtamap;
 
-import com.crashlytics.android.Crashlytics;
-import com.flurry.android.FlurryAgent;
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentManager;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.ActionBarActivity;
-import android.widget.FrameLayout;
+
+import com.crashlytics.android.Crashlytics;
+import com.xone.XoneManager;
+import com.xone.XoneTipStyle;
+
 import io.fabric.sdk.android.Fabric;
 
 public class MtaActivity extends ActionBarActivity {
+    private static final int REQUEST_CODE_ASK_PERMISSIONS = 1;
 
     /** Called when the activity is first created. */
     @Override
@@ -16,6 +23,13 @@ public class MtaActivity extends ActionBarActivity {
         super.onCreate(savedInstanceState);
         Fabric.with(this, new Crashlytics());
         setContentView(R.layout.main);
+
+        XoneManager.init(this, "3a550ccccdd44d968f68a0f6c27427b3");
+        XoneManager.enableAutoTips(this, XoneTipStyle.newBuilder().setShowFromBottom(true).build());
+
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) !=  PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, REQUEST_CODE_ASK_PERMISSIONS);
+        }
 
         FragmentManager fragmentManager = getSupportFragmentManager();
         fragmentManager.beginTransaction()
@@ -45,5 +59,20 @@ public class MtaActivity extends ActionBarActivity {
     protected void onStop(){
     	super.onStop();
         Analytics.onStop(this);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        switch (requestCode) {
+            case REQUEST_CODE_ASK_PERMISSIONS:
+                if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    XoneManager.locationPermissionGranted();
+                } else {
+                    XoneManager.locationPermissionDenied();
+                }
+                break;
+            default:
+                super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
     }
 }
