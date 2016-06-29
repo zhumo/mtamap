21cb4cac28ba20a4b7260c8b376b9d5646eb8946
Bilal Ahmed <bilal@appadvantage.co>
2016-06-29 16:01:25 +0500

    - Merged original code base - On line 8 of app/build.gradle, the app id is "com.subway.ladmin.subwaynyk" and I'd like it to be "com.rndapp.mtamap" - Version needs to be 18 and 3.0 in app/build.gradle - There are no "paid" or "free" flavors of the app, and there should be - In DbHelper's createDatabase method you call checkDataBase() twice. Is there a reason for that? - Please remove all Log and System.out statements - The max zoom on the map should be 6.0 - You've removed the ads from the application (both Admob and Yext), and those need to be put back - Analytics have also been removed, and should be put back - Fabric has been removed, and should be put back - Nagging has been removed, and should be put back - The link to the privacy policy (in the action bar menu) has been removed, and should be put back

---
+++ src/main/java/com/rndapp/mtamap/Stop.java
@@ -0,0 +1,24 @@
+package com.rndapp.mtamap;
+
+import java.io.Serializable;
+
+/**
+ * Created by ladmin on 02/06/2016.
+ */
+public class Stop implements Serializable {
+
+    public String name;
+    public String objectId;
+    public String parentId;
+
+    public Stop(String name, String objectId, String parentId) {
+        super();
+        this.name = name;
+        this.objectId = objectId;
+        this.parentId = parentId;
+    }
+
+    public Stop() {
+        super();
+    }
+}
